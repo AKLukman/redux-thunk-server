d0974c23a85cd56a8abf0b2a0b123367863644a5
AKLukman <abulkashemlukman@gmail.com>
2023-01-13 18:14:58 +0600

    commit

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.DATABASE_LOCAL;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     const db = client.db("moontech");
-    const productCollection = db.collection("product");
+    const productCollection = db.collection("products");
 
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
